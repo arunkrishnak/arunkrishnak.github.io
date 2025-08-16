f763457520bf95683acc38be7a8af2350f11886c
arunkrishnak <arunkrishnak2407@gmail.com>
2025-08-16 14:16:03 +0530

    UI Updated

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ const ageElement = document.getElementById('age');
 if (ageElement) {
     ageElement.textContent = calculateAge();
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
